185eea7a30f560ea4f934be2760e2653bd0f5049
nugosvianadze <nugzar.svianadze.1@btu.edu.ge>
2026-03-31 19:57:56 +0400

    added html templates, for login page, error page, users, added login endppint in flask, used flask forms, request, used request.args

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:63342"}})
 
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect(url_for("home"))
 
 users = [
     {"id": 1, "first_name": "John", "last_name": "Doe", "age": 28},
@@ -20,13 +27,14 @@ users = [
 @app.route("/home", methods=["GET"])
 @app.route("/")
 def home():
+    print(request.method)
     return render_template("index.html", users=users)
 
 
 # API endpoint
-@app.route("/get_user_data/<first_name>")
-@app.route("/get_user_data/<int:user_id>")
-@app.route("/get_user_data")
+@app.route("/api/get_user_data/<first_name>")
+@app.route("/api/get_user_data/<int:user_id>")
+@app.route("/api/get_user_data")
 def get_user_data(user_id: int = None, first_name: str = None):
     print(f'shemovida parametri : {user_id} = {first_name}')
     found_users = []
@@ -43,7 +51,7 @@ def get_user_data(user_id: int = None, first_name: str = None):
         return {
             'success': True,
             "users_data": users
-        }, 201
+        }, 200
 
 
     if not found_users and user_id:
@@ -59,7 +67,32 @@ def get_user_data(user_id: int = None, first_name: str = None):
     return {
         "success": True,
         "message": found_users
-    }, 201
+    }, 200
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    test_user_data = {
+        'email': "test@gmail.com",
+        'password': "123456"
+    }
+    if request.method == "GET":
+        return render_template("login.html")
+    print(request.form)
+    email, password = request.form.get('email'), request.form.get('password')
+    if email == test_user_data['email'] and password != test_user_data['password']:
+        return render_template("login.html", error="Password doesnt match!")
+    if email != test_user_data['email']:
+        return render_template("login.html", error="Account with this email doesnt exist!")
+    if email == test_user_data['email'] and password == test_user_data["password"]:
+        return redirect(url_for("home"))
+
+
+@app.route('/test_args')
+def test_args():
+    print(request.args)
+    print(request.args.get('arg1'))
+    return request.args
 
 
 if __name__ == "__main__":
