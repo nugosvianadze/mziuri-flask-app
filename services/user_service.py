5abe5b59edfa6df0acac80661d708fc004462b87
nugosvianadze <nugzar.svianadze.1@btu.edu.ge>
2026-04-02 20:08:04 +0400

    added user service as object for getting filtered users with query and id

---
+++ services/user_service.py
@@ -0,0 +1,20 @@
+class UserService:
+    @staticmethod
+    def get_users_with_word(word: str, users: list, found_users: list):
+        is_found = False
+        for user in users:
+            if user and word.lower() in f"{user["first_name"]} {user["last_name"]}".lower():
+                print('shemovida')
+                found_users.append(user)
+                is_found = True
+
+        if not is_found:
+            print('not found')
+            found_users = []
+
+
+    @staticmethod
+    def get_user_with_id(u_id: int, users: list) -> dict:
+        for user in users:
+            if u_id == user["id"]:
+                return user
