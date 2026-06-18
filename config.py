ccdd996e12fb203edb7a0bb6eb2e744fea888512
nugo <96307105+nugosvianadze@users.noreply.github.com>
2026-06-18 19:07:57 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,12 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-
+DATABASE_PATH = os.environ.get(
+    "DATABASE_PATH",
+    os.path.join(BASE_DIR, "mziuri.db")
+)
 
 class Config:
-    DB_PATH = os.path.join(BASE_DIR, "instance", "mziuri.db")
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_PATH}"
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{DATABASE_PATH}"
     SECURITY_PASSWORD_SALT = '146585145368132386173505678016728509634'
     REMEMBER_COOKIE_SAMESITE = "strict"
     SESSION_COOKIE_SAMESITE = "strict"
